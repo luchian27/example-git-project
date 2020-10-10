2b43c6170f22ce070f42c62d7073bd909eaf1c98
LUK <manole_lucian27@yahoo.com>
2020-10-10 15:26:51 +0300

    initial commit

---
+++ src/main/java/ro/tefacprogramator/m4/App.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
